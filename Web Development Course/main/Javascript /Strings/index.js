58dcc51f15a854e967bfc91a51c026fbc17a51e7
Prathamesh Zagade <prathamzagade1811@gmail.com>
2024-08-18 20:32:11 +0530

    Create index.js

---
+++ Web Development Course/main/Javascript /Strings/index.js
@@ -0,0 +1,27 @@
+console.log("This is strings tutorial")
+let a = "Anush";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+//console.log(a[5]) ////  Here we will get output as undefined
+
+console.log(a.length)
+
+let real_name = "hari";
+let friend = "om";
+
+console.log("his name is " + real_name + " and his friends name is " +friend)
+
+console.log(`his name is ${real_name} and friend is ${friend}`)
+
+let b = "Shivam"
+console.log(b.toUpperCase()) 
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(0,4)) 
+console.log(b.slice(1))
+
+console.log( b.replace("hiv",198))
+console.log(b.concat( a , "rahul"))
